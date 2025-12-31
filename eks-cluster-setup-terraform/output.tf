f5901d4b60de69e3b1be13311a96cec0a2823fad
bhavanapabbathi <60222708+bhavanapabbathi@users.noreply.github.com>
2025-12-31 15:13:11 +0530

    Create output.tf

---
+++ eks-cluster-setup-terraform/output.tf
@@ -0,0 +1,11 @@
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_oidc_issuer" {
+  value = module.eks.oidc_provider
+}
